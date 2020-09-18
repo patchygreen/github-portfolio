c77d0c680957e6cc4f44eea808ae495237fe0317
patchygreen <pocrean@gmail.com>
2020-09-18 16:59:18 +0100

    breaking out to components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,14 @@
 import React from "react";
 import "./Header.css";
+import Link from "../Link/Link";
 
-const Header = () => <header className="App-header"></header>;
+const Header = ({ logo }) => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <Link url="https://reactjs.org" title="Learn React" />
+  </header>
+);
 export default Header;
